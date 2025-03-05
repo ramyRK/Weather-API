ab2881249f3362f8417b27c841d277f2ca6b5eda
ramyRK <kheliliramy0@gmail.com>
2025-03-05 16:09:59 +0100

    finalizing

--- WeatherAPI/settings.py
+++ WeatherAPI/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+WEATHER_API_KEY = env('WEATHER_API_KEY')
+
 ALLOWED_HOSTS = []
 
 
